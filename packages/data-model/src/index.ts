b33b2767242786c29f59a7832789deb256d91538
ryuever <youchaoliu@xiaohongshu.com>
2023-05-25 14:17:15 +0800

    feat: update exports

--- packages/data-model/src/index.ts
+++ packages/data-model/src/index.ts
@@ -2,12 +2,11 @@ export { default as ItemsDimensions } from './ItemsDimensions';
 export { default as ListDimensions } from './ListDimensions';
 export { default as ListGroupDimensions } from './ListGroupDimensions';
 export { default as PseudoListDimensions } from './PseudoListDimensions';
-export * from './types/Dimensions.types';
 export { default as ItemMeta } from './ItemMeta';
 export { default as ListSpyUtils } from './utils/ListSpyUtils';
 export { default as Dimension } from './Dimension';
 
 export * from './viewable/viewabilityUtils';
 export * from './exportedUtils';
-export * from './types/onEndReachedHelper';
 export { default as ListSpy } from './utils/ListSpy';
+export * from './types';
