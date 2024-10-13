0a19dfa1bbc34eb6daf302c19c21d5b438626433
ryuyutyo <liuyouchao111@gmail.com>
2024-10-13 21:30:07 +0800

    feat: fail

--- packages/react-native/src/List.tsx
+++ packages/react-native/src/List.tsx
@@ -1,3 +1,22 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+window.React2 = React
+
+console.log('window react 2 ', window.React2)
+
+
+console.log('vi ', window.React1, window.React2, window.React1 === window.React2)
 export default () => {
-  return null;
+  console.log('in list')
+
+  return (
+    <View></View>
+  )
+
+  // return (
+  //   <View>
+  //     <Text>hello</Text>
+  //   </View>
+  // )
 };
