b809b98cd9b0be2a61229e9fcdbed58f916c50b7
ryuyutyo <liuyouchao111@gmail.com>
2024-10-20 14:11:40 +0800

    feat: update list

--- packages/react-native/src/index.ts
+++ packages/react-native/src/index.ts
@@ -1,16 +1,5 @@
 export { default as List } from './List';
-
-
-
 export { default as ListGroup } from './group/ListGroup';
 export { default as GroupList } from './group/GroupList';
 export { default as GroupListItem } from './group/GroupListItem';
-export { default as ListGroupContext } from './group/context'
-
-// const List = () => {}
-
-// export { List }
-
-// import List from './List'
-
-// export { List }
+export { default as ListGroupContext } from './group/context';
