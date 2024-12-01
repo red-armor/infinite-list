3dcfd20fcb901c8a5d5f4364e900a85e7a1518fd
ryuever <liuyouchao111@gmail.com>
2024-12-02 00:48:08 +0800

    feat: to trigger (#72)

--- packages/react-native/src/index.ts
+++ packages/react-native/src/index.ts
@@ -3,8 +3,8 @@ import { ScrollView } from './scrollView';
 export { default as List } from './list/List';
 export { default as ListGroup } from './group/ListGroup';
 export { default as GroupList } from './group/GroupList';
-export { default as GroupListItem } from './group/GroupListItem';
 export { default as ListGroupContext } from './group/context';
+export { default as GroupListItem } from './group/GroupListItem';
 
 export { ScrollView };
 export * from './scrollView';
