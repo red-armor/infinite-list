c63fc6a1f4a34cbebeb446061aa19c5d6a7679fd
ryuyutyo <liuyouchao111@gmail.com>
2024-12-03 01:50:32 +0800

    feat: bump expo 0.52

--- examples/ReactNativeListPlayground/app/_layout.tsx
+++ examples/ReactNativeListPlayground/app/_layout.tsx
@@ -1,4 +1,8 @@
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
+import {
+  DarkTheme,
+  DefaultTheme,
+  ThemeProvider,
+} from '@react-navigation/native';
 import { useFonts } from 'expo-font';
 import { Stack } from 'expo-router';
 import * as SplashScreen from 'expo-splash-screen';
@@ -13,7 +17,7 @@ SplashScreen.preventAutoHideAsync();
 export default function RootLayout() {
   const colorScheme = useColorScheme();
   const [loaded] = useFonts({
-    SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
+    SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'), // eslint-disable-line
   });
 
   useEffect(() => {
