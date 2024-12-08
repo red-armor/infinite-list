0f6693b09040caf5fcd45b931ebf4b3be9362527
ryuyutyo <liuyouchao111@gmail.com>
2024-12-08 22:04:26 +0800

    feat: init list

---
+++ ui/list/eslint.config.js
@@ -0,0 +1,23 @@
+const nx = require('@nx/eslint-plugin');
+const baseConfig = require('../../eslint.config.js');
+
+module.exports = [
+  ...baseConfig,
+  {
+    files: ['**/*.json'],
+    rules: {
+      '@nx/dependency-checks': [
+        'error',
+        { ignoredFiles: ['{projectRoot}/eslint.config.{js,cjs,mjs}'] },
+      ],
+    },
+    languageOptions: { parser: require('jsonc-eslint-parser') },
+  },
+  ...nx.configs['flat/react'],
+  {
+    files: ['**/*.ts', '**/*.tsx', '**/*.js', '**/*.jsx'],
+    // Override or add rules here
+    rules: {},
+  },
+  { ignores: ['public', '.cache', 'node_modules'] },
+];
