8348078b6a5f915554925af59dd45017465d1b2a
ryuyutyo <liuyouchao111@gmail.com>
2024-12-22 16:16:02 +0800

    feat: init item-meta

---
+++ core/item-meta/src/ItemMeta.ts
@@ -0,0 +1,394 @@
+// import BaseDimensions from './BaseDimensions';
+// import Dimension from './Dimension';
+import { BaseDimensions, Dimension } from './types';
+
+import ItemMetaStateEventHelper from './ItemMetaStateEventHelper';
+import { DEFAULT_LAYOUT, DEFAULT_RECYCLER_TYPE } from './common';
+import {
+  ItemLayout,
+  ItemMetaOwner,
+  ItemMetaState,
+  ItemMetaProps,
+  StateEventListener,
+  ItemMetaStateEventHelperProps,
+  GenericItemT,
+} from './types';
+import noop from '@x-oasis/noop';
+import defaultBooleanValue from '@x-oasis/default-boolean-value';
+import { ViewabilityItemMeta } from '@infinite-list/viewable';
+
+export const isValidMetaLayout = (meta: ItemMeta | null | undefined) =>
+  !!(meta && !meta.isApproximateLayout && meta.getLayout());
+
+type ItemMetaContext<T extends GenericItemT = GenericItemT> = {
+  [key: string]: ItemMeta<T>;
+};
+
+// make itemMeta could be shared, such as data source ref change, but it's value
+// not changed.
+export let context: ItemMetaContext = {};
+
+let count = 0;
+
+export const resetContext = () => {
+  context = {};
+};
+
+/**
+ * _layout should always exist. `_isApproximateLayout` should make the difference.
+ */
+class ItemMeta<
+  ItemT extends GenericItemT = GenericItemT
+> extends ViewabilityItemMeta {
+  private _isListItem: boolean;
+  private _id: string;
+  private _layout?: ItemLayout;
+  private _separatorLength?: number;
+  private _recyclerType: string;
+  private _owner: ItemMetaOwner<ItemT>;
+  private _state: ItemMetaState;
+  private _stateEventSubscriptions: Map<string, ItemMetaStateEventHelper>;
+  readonly getMetaOnViewableItemsChanged?: any;
+  readonly _canIUseRIC?: boolean;
+  private _isApproximateLayout: boolean;
+  private _ignoredToPerBatch: boolean;
+  private _useSeparatorLength: boolean;
+  private _spawnProps: {
+    [key: string]: ItemMetaStateEventHelperProps;
+  };
+
+  constructor(props: ItemMetaProps<ItemT>) {
+    super(props);
+    const {
+      owner,
+      separatorLength,
+      layout,
+      state,
+      isListItem,
+      canIUseRIC,
+      spawnProps = {},
+      ignoredToPerBatch,
+      isInitialItem = false,
+      isApproximateLayout = true,
+
+      useSeparatorLength = true,
+
+      recyclerType = DEFAULT_RECYCLER_TYPE,
+    } = props;
+    this._owner = owner;
+    this._id = `item_meta_${count++}`;
+    this._layout = layout;
+    this._separatorLength = separatorLength || 0;
+    this._isListItem = isListItem || false;
+    this._stateEventSubscriptions = new Map();
+    this._ignoredToPerBatch = !!ignoredToPerBatch;
+    this._useSeparatorLength = useSeparatorLength;
+    this._state =
+      state || this._owner?.resolveConfigTuplesDefaultState
+        ? this._owner?.resolveConfigTuplesDefaultState(!!isInitialItem)
+        : {};
+
+    this._canIUseRIC = canIUseRIC;
+    this._isApproximateLayout = isApproximateLayout;
+    this._recyclerType = recyclerType;
+    this._spawnProps = spawnProps;
+
+    this.addStateEventListener = this.addStateEventListener.bind(this);
+    (context as any)[this.key] = this;
+  }
+
+  static spawn<T extends GenericItemT = GenericItemT>(props: ItemMetaProps<T>) {
+    const ancestor = context[props.key];
+    if (ancestor) {
+      const layout = ancestor.getLayout();
+      const spawnProps: {
+        [key: string]: any;
+      } = {};
+      for (const [key, value] of ancestor._stateEventSubscriptions) {
+        const _props = ItemMetaStateEventHelper.spawn(value);
+        if (_props) spawnProps[key] = _props;
+      }
+
+      return new ItemMeta({
+        layout,
+        // isApproximateLayout may cause change...
+        isApproximateLayout: ancestor.isApproximateLayout,
+        spawnProps,
+        ...props,
+      });
+    }
+
+    return new ItemMeta(props);
+  }
+
+  get id() {
+    return this._id;
+  }
+
+  get ignoredToPerBatch() {
+    return this._ignoredToPerBatch;
+  }
+
+  get recyclerType() {
+    return this._recyclerType;
+  }
+
+  get isApproximateLayout() {
+    return this._isApproximateLayout;
+  }
+
+  set isApproximateLayout(value: boolean) {
+    this._isApproximateLayout = value;
+  }
+
+  setLayout(layout: ItemLayout) {
+    this._layout = layout;
+  }
+
+  getLayout() {
+    return this._layout;
+  }
+
+  getOwner() {
+    return this._owner;
+  }
+
+  getState() {
+    return this._state;
+  }
+
+  ensureLayout() {
+    if (this._layout) return this._layout;
+    this._layout = { ...DEFAULT_LAYOUT };
+    return this._layout;
+  }
+
+  setSeparatorLength(length: number) {
+    this._separatorLength = length;
+  }
+
+  getSeparatorLength() {
+    return this._separatorLength || 0;
+  }
+
+  getContainerOffset() {
+    return this._owner.getContainerOffset();
+  }
+
+  getItemLength() {
+    const selectValue = this._owner.getSelectValue();
+    return this._layout ? selectValue.selectLength(this._layout) : 0;
+  }
+
+  setUseSeparatorLength(value: boolean) {
+    this._useSeparatorLength = value;
+  }
+
+  getFinalItemLength(useSeparatorLength = false) {
+    if (useSeparatorLength || this._useSeparatorLength) {
+      return this.getItemLength() + this.getSeparatorLength();
+    }
+
+    return this.getItemLength();
+  }
+
+  getItemOffset(exclusive?: boolean) {
+    if (this._isListItem) {
+      // @ts-ignore [TODO]
+      const offset = (this._owner as BaseDimensions<ItemT>).getKeyItemOffset(
+        this._key,
+        exclusive
+      );
+
+      return offset;
+    }
+
+    // @ts-ignore [TODO] for dimension
+    if (this._owner instanceof Dimension) {
+      return this._owner.getItemOffset();
+    }
+
+    const selectValue = this._owner.getSelectValue();
+    return this._layout ? selectValue.selectOffset(this._layout) : 0;
+  }
+
+  /**
+   *
+   * @param state
+   * @returns
+   *
+   * trigger state change listener, such as viewable / impression
+   */
+  setItemMetaState(
+    state: {
+      [key: string]: boolean;
+    } = {}
+  ) {
+    if (state === this._state) return;
+    Object.keys({ ...state }).forEach((key) => {
+      const helper = this._stateEventSubscriptions.get(key);
+      const currentValue = state[key];
+
+      if (helper) {
+        helper.setValue(currentValue);
+      }
+
+      if (key === 'viewable') {
+        const impressionHelper =
+          this._stateEventSubscriptions.get('impression');
+        impressionHelper?.trigger(currentValue);
+      }
+    });
+
+    this._state = { ...state };
+  }
+
+  override getKey() {
+    return this._key;
+  }
+
+  ensureStateHelper(
+    eventName: string,
+    value: boolean
+  ): ItemMetaStateEventHelper {
+    if (!this._stateEventSubscriptions.get(eventName)) {
+      const helper = new ItemMetaStateEventHelper({
+        ...(this._spawnProps[eventName] || {}),
+        eventName,
+        key: this._key,
+        defaultValue: value,
+        canIUseRIC: this._canIUseRIC,
+      });
+      this._stateEventSubscriptions.set(eventName, helper);
+    }
+
+    return this._stateEventSubscriptions.get(eventName)!;
+  }
+
+  getIndexInfo() {
+    return this._owner.getIndexInfo(this._key);
+  }
+
+  override getIndex() {
+    const info = this.getIndexInfo();
+    return info?.index || -1;
+  }
+
+  /**
+   *
+   * @param event
+   * @param callback
+   * @param triggerOnceIfTrue
+   *
+   * In reuse condition, once add listener, then it will not be changed anymore.
+   *
+   */
+  addStateReusableEventListener(
+    event: string,
+    key: string,
+    callback: StateEventListener,
+    triggerOnceIfTrue?: boolean
+  ): {
+    remover: Function;
+  } {
+    if (typeof callback !== 'function')
+      return {
+        remover: noop,
+      };
+    const stateEventHelper = this.ensureStateHelper(
+      event,
+      // get initial value
+      event === 'impression' ? this._state['viewable'] : this._state[event]
+    );
+    return stateEventHelper.addReusableListener(
+      callback,
+      key,
+      defaultBooleanValue(triggerOnceIfTrue, true)
+    );
+  }
+
+  /**
+   *
+   * @param event
+   * @param callback
+   * @param triggerOnceIfTrue
+   *
+   * In reuse condition, once add listener, then it will not be changed anymore.
+   *
+   */
+  addStStateReusableEventListener(
+    event: string,
+    key: string,
+    callback: StateEventListener,
+    triggerOnceIfTrue?: boolean
+  ): {
+    remover: Function;
+  } {
+    if (typeof callback !== 'function')
+      return {
+        remover: noop,
+      };
+    const stateEventHelper = this.ensureStateHelper(
+      event,
+      // get initial value
+      event === 'impression' ? this._state['viewable'] : this._state[event]
+    );
+    return stateEventHelper.addReusableListener(
+      callback,
+      key,
+      defaultBooleanValue(triggerOnceIfTrue, true)
+    );
+  }
+
+  /**
+   *
+   * @param event
+   * @param callback
+   * @param triggerOnceIfTrue
+   *
+   * In reuse condition, once add listener, then it will not be changed anymore.
+   *
+   */
+  addStrictStateReusableEventListener(
+    event: string,
+    key: string,
+    callback: StateEventListener,
+    triggerOnceIfTrue?: boolean
+  ): {
+    remover: Function;
+  } {
+    if (typeof callback !== 'function')
+      return {
+        remover: noop,
+      };
+    const stateEventHelper = this.ensureStateHelper(
+      event,
+      // get initial value
+      event === 'impression' ? this._state['viewable'] : this._state[event]
+    );
+    return stateEventHelper?.addStrictReusableListener(
+      callback,
+      key,
+      defaultBooleanValue(triggerOnceIfTrue, true)
+    );
+  }
+
+  addStateEventListener(
+    event: string,
+    callback: StateEventListener,
+    triggerOnceIfTrue?: boolean
+  ) {
+    if (typeof callback !== 'function') return noop;
+    const stateEventHelper = this.ensureStateHelper(
+      event,
+      // get initial value
+      event === 'impression' ? this._state['viewable'] : this._state[event]
+    );
+    return stateEventHelper?.addListener(
+      callback,
+      defaultBooleanValue(triggerOnceIfTrue, true)
+    );
+  }
+}
+
+export default ItemMeta;
---
+++ core/item-meta/src/ItemMetaStateEventHelper.ts
@@ -0,0 +1,354 @@
+import Batchinator from '@x-oasis/batchinator';
+import defaultBooleanValue from '@x-oasis/default-boolean-value';
+import noop from '@x-oasis/noop';
+import getMapKeyByValue from '@x-oasis/get-map-key-by-value';
+import { StateEventListener, ItemMetaStateEventHelperProps } from './types';
+
+let canIUseRIC = false;
+let finished = false;
+
+setTimeout(() => {
+  if (finished) return;
+  canIUseRIC = false;
+  finished = true;
+});
+
+// interface IdleRequestCallback {
+//   (deadline: IdleDeadline): void;
+// }
+/** [MDN Reference](https://developer.mozilla.org/docs/Web/API/IdleDeadline) */
+// interface IdleDeadline {
+//   /** [MDN Reference](https://developer.mozilla.org/docs/Web/API/IdleDeadline/didTimeout) */
+//   readonly didTimeout: boolean;
+//   /** [MDN Reference](https://developer.mozilla.org/docs/Web/API/IdleDeadline/timeRemaining) */
+//   timeRemaining(): DOMHighResTimeStamp;
+// }
+// interface IdleRequestOptions {
+//   timeout?: number;
+// }
+
+// declare function requestIdleCallback(
+//   callback: IdleRequestCallback,
+//   options?: IdleRequestOptions
+// ): number;
+
+// if (requestIdleCallback)
+//   requestIdleCallback(() => {
+//     canIUseRIC = true;
+//     finished = true;
+//   });
+
+class ItemMetaStateEventHelper {
+  private _batchUpdateEnabled: boolean;
+  private _value = false;
+  readonly _eventName: string;
+
+  private _strictListeners: Array<StateEventListener>;
+
+  private _listeners: Array<StateEventListener> = [];
+  private _handleCountMap: Map<StateEventListener, number>;
+
+  private _triggerBatchinator: Batchinator;
+  private _once: boolean;
+  readonly _key: string;
+  private _reusableEventListenerMap: Map<string, StateEventListener>;
+  private _reusableStrictEventListenerMap: Map<string, StateEventListener>;
+  private _callbackId?: number | void;
+  private _callbackStartMinMs?: number;
+  readonly _canIUseRIC: boolean;
+  private _strictListenerKeyToHandleCountMap: {
+    [key: string]: number;
+  };
+
+  constructor(props: ItemMetaStateEventHelperProps) {
+    const {
+      key,
+      eventName,
+      batchUpdateEnabled,
+      defaultValue = false,
+      canIUseRIC: _canIUseRIC,
+      once,
+      strictListenerKeyToHandleCountMap = {},
+    } = props;
+
+    this._key = key;
+    this._eventName = eventName;
+    this._reusableEventListenerMap = new Map();
+    this._reusableStrictEventListenerMap = new Map();
+    this._batchUpdateEnabled =
+      typeof batchUpdateEnabled === 'boolean'
+        ? batchUpdateEnabled
+        : eventName === 'viewable'
+        ? false
+        : true;
+    this._once =
+      typeof once === 'boolean'
+        ? once
+        : eventName === 'impression'
+        ? true
+        : false;
+
+    this._strictListenerKeyToHandleCountMap = strictListenerKeyToHandleCountMap;
+    this._triggerBatchinator = new Batchinator(this._trigger.bind(this), 50);
+    this.remover = this.remover.bind(this);
+    this._handleCountMap = new Map();
+    this._strictListeners = [];
+    // ric in RN can not be triggered. https://github.com/facebook/react-native/issues/28602
+    this._canIUseRIC = defaultBooleanValue(_canIUseRIC, canIUseRIC);
+
+    if (defaultValue) {
+      this.trigger(defaultValue);
+    }
+  }
+
+  static spawn(ins: ItemMetaStateEventHelper) {
+    const strictListenerKeyToHandleCountMap = Object.create(null);
+    const l = ins._strictListeners;
+    if (!l.length) return null;
+    l.forEach((_l) => {
+      const count = ins._handleCountMap.get(_l);
+      for (const [key, value] of ins._reusableEventListenerMap) {
+        if (value === _l) {
+          strictListenerKeyToHandleCountMap[key] = count;
+        }
+      }
+    });
+    return {
+      strictListenerKeyToHandleCountMap,
+    };
+  }
+
+  remover(listener: StateEventListener, key?: string) {
+    return () => {
+      const index = this._listeners.findIndex((cb) => cb === listener);
+      if (index !== -1) {
+        this._listeners.splice(index, 1);
+        this._handleCountMap.delete(listener);
+      }
+      if (key) this._reusableEventListenerMap.delete(key);
+    };
+  }
+
+  addReusableListener(
+    listener: StateEventListener,
+    key: string,
+    triggerOnceIfTrue: boolean
+  ) {
+    if (this._reusableEventListenerMap.has(key))
+      return {
+        remover: this.remover(listener, key),
+      };
+
+    this._reusableEventListenerMap.set(key, listener);
+
+    const index = this._listeners.findIndex((cb) => cb === listener);
+    if (index === -1) {
+      this._handleCountMap.set(listener, 0);
+      this._listeners.push(listener);
+    }
+
+    if (triggerOnceIfTrue && this._value && this.listenerGuard(listener)) {
+      this.incrementHandleCount(listener);
+      listener(this._value);
+    }
+
+    return {
+      remover: this.remover(listener, key),
+    };
+  }
+
+  // listener can't be deleted
+  addStrictReusableListener(
+    listener: StateEventListener,
+    key: string,
+    triggerOnceIfTrue: boolean
+  ) {
+    const count = this._strictListenerKeyToHandleCountMap[key] || 0;
+    const index = this._strictListeners.findIndex((cb) => cb === listener);
+
+    const prevListener = this._reusableStrictEventListenerMap.get(key);
+    if (prevListener) {
+      this._handleCountMap.delete(listener);
+      const idx = this._strictListeners.findIndex((l) => l === prevListener);
+      if (idx !== -1) this._strictListeners.splice(idx, 1);
+    }
+
+    // should set before trigger
+    this._handleCountMap.set(listener, count);
+
+    if (index === -1) {
+      this._strictListeners.push(listener);
+      if (triggerOnceIfTrue && this._value && this.listenerGuard(listener)) {
+        this.incrementHandleCount(listener);
+        listener(this._value);
+      }
+    }
+
+    this._reusableStrictEventListenerMap.set(key, listener);
+
+    if (triggerOnceIfTrue && this._value && this.listenerGuard(listener)) {
+      this.incrementStrictListenerHandleCount(listener);
+      listener(this._value);
+    }
+
+    return {
+      remover: noop,
+    };
+  }
+
+  /**
+   *
+   * @param listener
+   * @param triggerOnceIfTrue on initial, if value is true, then trigger..
+   * @returns
+   */
+  addListener(listener: StateEventListener, triggerOnceIfTrue: boolean) {
+    const index = this._listeners.findIndex((cb) => cb === listener);
+    if (index === -1) {
+      this._handleCountMap.set(listener, 0);
+      this._listeners.push(listener);
+    }
+
+    if (triggerOnceIfTrue && this._value && this.listenerGuard(listener)) {
+      this.incrementHandleCount(listener);
+      listener(this._value);
+    }
+
+    return this.remover(listener);
+  }
+
+  getHandleCount(handler: StateEventListener) {
+    return this._handleCountMap.get(handler) || 0;
+  }
+
+  incrementHandleCount(handler: StateEventListener) {
+    const value = this._handleCountMap.get(handler) || 0;
+    this._handleCountMap.set(handler, value + 1);
+  }
+
+  incrementStrictListenerHandleCount(handler: StateEventListener) {
+    const value = this._handleCountMap.get(handler) || 0;
+    this._handleCountMap.set(handler, value + 1);
+    const key = getMapKeyByValue(this._reusableStrictEventListenerMap, handler);
+    if (key) {
+      const _value = this._strictListenerKeyToHandleCountMap[key] || 0;
+      this._strictListenerKeyToHandleCountMap[key] = _value + 1;
+    }
+  }
+
+  get value() {
+    return this._value;
+  }
+
+  setValue(value: boolean) {
+    this.trigger(value);
+  }
+
+  // 大的开关，是否要跑trigger逻辑
+  guard() {
+    if (!this._once) return true;
+    // has a listener still not triggered.
+    for (const value of this._handleCountMap.values()) {
+      if (!value) return true;
+    }
+    return false;
+  }
+
+  /**
+   *
+   * @param cb
+   * @returns to control whether a listener should be triggered or not..
+   * 用来卡，某一个listener是不是需要被触发
+   */
+  listenerGuard(cb: StateEventListener) {
+    if (!this._once) return true;
+    if (this._handleCountMap.get(cb)) return false;
+    return true;
+  }
+
+  cancelIdleCallbackPolyfill(callbackId: number) {
+    if (this._canIUseRIC) {
+      if (typeof cancelIdleCallback === 'function') {
+        cancelIdleCallback(callbackId);
+      }
+    }
+
+    this._callbackId = undefined;
+  }
+
+  trigger(value: boolean) {
+    const shouldPerformScheduler = this.guard();
+    if (!shouldPerformScheduler) return;
+    if (value && !this._batchUpdateEnabled) {
+      this._trigger(value, true);
+      return;
+    }
+
+    this._triggerBatchinator.dispose({
+      abort: true,
+    });
+    this._triggerBatchinator.schedule(value);
+  }
+
+  get listeners() {
+    const arr: {
+      type: 'normal' | 'strict';
+      listener: StateEventListener;
+    }[] = [];
+    this._listeners.forEach((listener) =>
+      arr.push({ type: 'normal', listener })
+    );
+    this._strictListeners.forEach((listener) =>
+      arr.push({ type: 'strict', listener })
+    );
+    return arr;
+  }
+
+  _trigger(value: boolean, immediately: boolean) {
+    const now = Date.now();
+
+    if (immediately) {
+      this._callbackStartMinMs = now;
+      if (this._callbackId) {
+        this.cancelIdleCallbackPolyfill(this._callbackId);
+      }
+      if (this._value !== value) {
+        this.listeners.forEach(({ listener, type }) => {
+          if (this.listenerGuard(listener)) {
+            if (type === 'strict')
+              this.incrementStrictListenerHandleCount(listener);
+            else this.incrementHandleCount(listener);
+            listener(value);
+          }
+        });
+      }
+      this._value = value;
+    } else {
+      if (this._callbackId) {
+        this.cancelIdleCallbackPolyfill(this._callbackId);
+      }
+      this._callbackStartMinMs = now;
+
+      const handler = (() => {
+        if (now < this._callbackStartMinMs!) return;
+        if (this._value !== value) {
+          this.listeners.forEach(({ listener, type }) => {
+            if (this.listenerGuard(listener)) {
+              if (type === 'strict')
+                this.incrementStrictListenerHandleCount(listener);
+              else this.incrementHandleCount(listener);
+              listener(value);
+            }
+          });
+        }
+        this._value = value;
+      }).bind(this);
+
+      this._callbackId = this._canIUseRIC
+        ? requestIdleCallback(handler)
+        : handler();
+    }
+  }
+}
+
+export default ItemMetaStateEventHelper;
