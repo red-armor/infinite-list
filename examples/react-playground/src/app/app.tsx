be5ef5e22017bfb251ead5d776222039af146655
ryuyutyo <liuyouchao111@gmail.com>
2024-12-24 00:26:21 +0800

    feat: add react-playground

---
+++ examples/react-playground/src/app/app.tsx
@@ -0,0 +1,13 @@
+import styles from './app.module.css';
+
+import NxWelcome from './nx-welcome';
+
+export function App() {
+  return (
+    <div>
+      <NxWelcome title="react-playground" />
+    </div>
+  );
+}
+
+export default App;
