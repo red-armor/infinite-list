e791da686660b6b51956e280eabdec1bd2a6cea4
ryuyutyo <liuyouchao111@gmail.com>
2024-12-27 18:38:42 +0800

    feat: update

--- ui/group/src/common/PortalContent.tsx
+++ ui/group/src/common/PortalContent.tsx
@@ -16,112 +16,108 @@ import {
 
 import GroupListItemImpl from './GroupListItemImpl';
 
-const MemoedRecycleContentItem = genericMemo(
-  <IStyle, ItemT extends GenericItemT>(
-    props: RecycleContentItem<IStyle, ItemT>
-  ) => {
-    const {
-      listKey,
-      itemMeta,
-      dimensions,
-      item,
-      offset,
-      containerKey,
-      horizontal,
-      RecycleContentItemWrapper,
-    } = props;
+const RecycleContentItem = <IStyle, ItemT extends GenericItemT>(
+  props: RecycleContentItem<IStyle, ItemT>
+) => {
+  const {
+    listKey,
+    itemMeta,
+    dimensions,
+    item,
+    offset,
+    containerKey,
+    horizontal,
+    RecycleContentItemWrapper,
+  } = props;
 
-    const containerStyle = useMemo(
-      () =>
-        horizontal
-          ? {
-              position: 'absolute',
-              left: offset,
-              top: 0,
-              bottom: 0,
-            }
-          : {
-              position: 'absolute',
-              top: offset,
-              left: 0,
-              right: 0,
-            },
-      [offset]
-    );
+  const containerStyle = useMemo(
+    () =>
+      horizontal
+        ? {
+            position: 'absolute',
+            left: offset,
+            top: 0,
+            bottom: 0,
+          }
+        : {
+            position: 'absolute',
+            top: offset,
+            left: 0,
+            right: 0,
+          },
+    [offset]
+  );
 
-    return (
-      <RecycleContentItemWrapper style={containerStyle}>
-        <GroupListItemImpl
-          item={item}
-          itemKey={listKey}
-          itemMeta={itemMeta}
-          renderItem={itemMeta.getOwner().renderItem}
-          teleportItemProps={itemMeta.getOwner().teleportItemProps}
-          containerKey={containerKey}
-          dimensions={dimensions}
-        />
-      </RecycleContentItemWrapper>
-    );
-  }
-);
+  return (
+    <RecycleContentItemWrapper style={containerStyle}>
+      <GroupListItemImpl
+        item={item}
+        itemKey={listKey}
+        itemMeta={itemMeta}
+        renderItem={itemMeta.getOwner().renderItem}
+        teleportItemProps={itemMeta.getOwner().teleportItemProps}
+        containerKey={containerKey}
+        dimensions={dimensions}
+      />
+    </RecycleContentItemWrapper>
+  );
+};
+const MemoedRecycleContentItem = memo(
+  RecycleContentItem
+) as typeof RecycleContentItem;
 
-const MemoedRecycleContent = genericMemo(
-  <IStyle, ItemT extends GenericItemT>(
-    props: GroupRecycleContentProps<IStyle, ItemT>
-  ) => {
-    const { state, RecycleContentItemWrapper, ...rest } = props;
-    return (
-      <>
-        {state.map((stateResult) => {
-          const { key, itemMeta, ...stateResultRest } = stateResult;
-          return (
-            <MemoedRecycleContentItem
-              key={key}
-              containerKey={key}
-              renderItem={itemMeta!.getOwner().renderItem}
-              RecycleContentItemWrapper={RecycleContentItemWrapper}
-              itemMeta={itemMeta}
-              {...rest}
-              {...stateResultRest}
-            />
-          );
-        })}
-      </>
-    );
-  },
-  (prev, next) => prev.state === next.state
-);
+const RecycleContent = <IStyle, ItemT extends GenericItemT>(
+  props: GroupRecycleContentProps<IStyle, ItemT>
+) => {
+  const { state, RecycleContentItemWrapper, ...rest } = props;
+  return (
+    <>
+      {state.map((stateResult) => {
+        const { key, itemMeta, ...stateResultRest } = stateResult;
+        return (
+          <MemoedRecycleContentItem<IStyle, ItemT>
+            key={key}
+            containerKey={key}
+            renderItem={itemMeta!.getOwner().renderItem}
+            RecycleContentItemWrapper={RecycleContentItemWrapper}
+            itemMeta={itemMeta!}
+            {...rest}
+            {...stateResultRest}
+          />
+        );
+      })}
+    </>
+  );
+};
+const MemoedRecycleContent = memo(RecycleContent) as typeof RecycleContent;
 
-const MemoedSpaceContent = genericMemo(
-  <IStyle, ItemT extends GenericItemT>(
-    props: GroupSpaceContentProps<IStyle, ItemT>
-  ) => {
-    const { state, listKey, dimensions, SpaceRendererComponent } = props;
+const SpaceContent = <IStyle, ItemT extends GenericItemT>(
+  props: GroupSpaceContentProps<IStyle, ItemT>
+) => {
+  const { state, listKey, dimensions, SpaceRendererComponent } = props;
 
-    return (
-      <>
-        {state.map((stateResult, index) => {
-          const { isSpace, key, item, length, isSticky, itemMeta } =
-            stateResult;
-          return isSpace ? (
-            <SpaceRendererComponent key={key} style={{ height: length }} />
-          ) : (
-            <GroupListItemImpl<ItemT>
-              item={item!}
-              key={key}
-              itemKey={listKey}
-              itemMeta={itemMeta!}
-              renderItem={itemMeta!.getOwner().renderItem}
-              teleportItemProps={itemMeta!.getOwner().teleportItemProps}
-              dimensions={dimensions}
-            />
-          );
-        })}
-      </>
-    );
-  },
-  (prev, next) => prev.state === next.state
-);
+  return (
+    <>
+      {state.map((stateResult, index) => {
+        const { isSpace, key, item, length, isSticky, itemMeta } = stateResult;
+        return isSpace ? (
+          <SpaceRendererComponent key={key} style={{ height: length }} />
+        ) : (
+          <GroupListItemImpl<ItemT>
+            item={item!}
+            key={key}
+            itemKey={listKey}
+            itemMeta={itemMeta!}
+            renderItem={itemMeta!.getOwner().renderItem}
+            teleportItemProps={itemMeta!.getOwner().teleportItemProps}
+            dimensions={dimensions}
+          />
+        );
+      })}
+    </>
+  );
+};
+const MemoedSpaceContent = memo(SpaceContent) as typeof SpaceContent;
 
 const PortalContent = <IStyle, ItemT extends GenericItemT>(
   props: PropsWithChildren<PortalContextProps<IStyle, ItemT>>
@@ -149,7 +145,7 @@ const PortalContent = <IStyle, ItemT extends GenericItemT>(
 
   return (
     <>
-      <MemoedSpaceContent
+      <MemoedSpaceContent<IStyle, ItemT>
         listKey={id}
         ownerId={id}
         state={store.spaceState}
