b79b10cc446076cb658444e7abe2f06f2047e95b
ryuyutyo <liuyouchao111@gmail.com>
2025-02-06 23:54:02 +0800

    feat: update

--- core/intersection-observer/rollup.config.js
+++ core/intersection-observer/rollup.config.js
@@ -2,7 +2,7 @@ const { withNx } = require('@nx/rollup/with-nx');
 
 module.exports = withNx(
   {
-    additionalEntryPoints: [],
+    // additionalEntryPoints: [],
     outputPath: '../../dist/core/intersection-observer',
     tsConfig: './tsconfig.lib.json',
     compiler: 'babel',
