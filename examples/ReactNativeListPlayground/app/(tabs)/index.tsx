c571d4bac6c2a58a6b719cff2c21eb5e2c28cf33
ryuyutyo <liuyouchao111@gmail.com>
2025-02-09 15:10:44 +0800

    feat: update

--- examples/ReactNativeListPlayground/app/(tabs)/index.tsx
+++ examples/ReactNativeListPlayground/app/(tabs)/index.tsx
@@ -164,16 +164,18 @@
 //   );
 // };
 
-// import List from '../../components/play/List';
+import List from '../../components/play/List';
 // import MasonryList from '../../components/play/MasonryList';
 // import Group from '../../components/play/Group';
 // import HorizontalList from '../../components/play/HorizontalList';
 // import HorizontalListWithContainer from '../../components/play/HorizontalListWithContainer';
 // import MeasureInWindowSimple from '../../components/play/MeasureInWindowSimple';
 // import MeasureInWindowSimpleIntersection from '../../components/play/MeasureInWindowSimpleIntersection';
-import MeasureInWindowSimpleIntersectionScrollView from '../../components/play/MeasureInWindowSimpleIntersectionScrollView';
+// import MeasureInWindowSimpleIntersectionScrollView from '../../components/play/MeasureInWindowSimpleIntersectionScrollView';
 // import StickyViewFluid from '../../components/play/StickyViewFluid';
 // import StickyViewStuck from '../../components/play/StickyViewStuck';
+
+import IntersectionObserverList from '../../components/play/IntersectionObserverList';
 
 export default () => {
   // return <List />;
@@ -184,7 +186,8 @@ export default () => {
 
   // return <MeasureInWindowSimple />;
   // return <MeasureInWindowSimpleIntersection />;
-  return <MeasureInWindowSimpleIntersectionScrollView />;
+  // return <MeasureInWindowSimpleIntersectionScrollView />;
   // return <StickyViewFluid />;
   // return <StickyViewStuck />;
+  return <IntersectionObserverList />;
 };
