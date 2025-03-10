eee16b66099fc5955223953faeed25b76699c371
ryuyutyo <liuyouchao111@gmail.com>
2025-03-10 23:49:08 +0800

    feat: update

--- ui/scroller/src/react-native/ScrollView.tsx
+++ ui/scroller/src/react-native/ScrollView.tsx
@@ -9,8 +9,10 @@ import React, {
   useRef,
 } from 'react';
 import { ScrollView as RNScrollView, View as RNView } from 'react-native';
+import isRefObject from '@x-oasis/is-ref';
 import Marshal from './Marshal';
 import { DEFAULT_SCROLL_EVENT_THROTTLE } from './commons/constants';
+import { resolveScrollViewKey } from './commons/utils';
 import AnimatedRenderer from './component/AnimatedRenderer';
 import BasicRenderer from './component/BasicRenderer';
 import { defaultViewabilityConfigCallbackPairs } from './constants';
@@ -19,13 +21,11 @@ import FooterPortalContainer from './portal/FooterContainer';
 import HeaderPortalContainer from './portal/HeaderContainer';
 import PortalManager from './portal/Manager';
 import {
-  ScrollToOption,
   InfiniteListScrollViewProps,
   InfiniteListScrollViewPropsWithForwardRef,
   InfiniteListScrollViewPropsWithRef,
+  ScrollToOption,
 } from './types';
-import isRefObject from '@x-oasis/is-ref';
-import { resolveScrollViewKey } from './commons/utils';
 
 const ScrollView: FC<InfiniteListScrollViewPropsWithForwardRef> = (props) => {
   const {
