2684db4e98b6354817aff863af759c63c4fb8d42
ryuyutyo <liuyouchao111@gmail.com>
2025-07-17 00:29:45 +0800

    feat: update config

---
+++ examples/ReactNativeInfiniteList/metro.config.js
@@ -0,0 +1,35 @@
+const path = require('path');
+const { resolveModules } = require('@infinite-list/metro-infinite-list-resolver');
+const { getDefaultConfig } = require('expo/metro-config');
+
+const config = getDefaultConfig(__dirname);
+
+const mapping = resolveModules({
+  rootPath: path.join(__dirname, 'node_modules'),
+  targetDir: '@infinite-list',
+})
+
+
+config.resolver.extraNodeModules = {
+  ...mapping,
+};
+
+const { resolver } = config;
+config.resolver = {
+  ...resolver,
+  resolveRequest: (context, moduleName, platform, realModuleName) => {
+    for (const k in mapping) {
+      if (moduleName === k) {
+        return {
+          filePath: mapping[k],
+          type: 'sourceFile',
+        }
+      }
+    }
+    return context.resolveRequest(context, moduleName, platform);
+  },
+};
+
+console.log('confing ==== ', config.resolver.extraNodeModules);
+
+module.exports = config;
