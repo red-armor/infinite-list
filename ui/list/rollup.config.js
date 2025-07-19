60d1e237c4675ea3705ab51226fd8564286d0b46
ryuyutyo <liuyouchao111@gmail.com>
2025-07-19 13:39:53 +0800

    fix: list buld issue

--- ui/list/rollup.config.js
+++ ui/list/rollup.config.js
@@ -1,44 +1,14 @@
-const { withNx } = require('@nx/rollup/with-nx');
-
-module.exports = withNx(
-  {
-    // main: './src/index.ts',
-
-    // may cause
-    additionalEntryPoints: [],
-    outputPath: '../../dist/ui/list',
-    tsConfig: './tsconfig.lib.json',
-    compiler: 'babel',
-    format: ['esm'],
-    assets: [{ input: '.', output: '.', glob: '*.md' }],
-  },
-  {
-    input: ['./src/react-native/index.ts', './src/react/index.ts'],
-    output: {
-      entryFileNames: (chunk) => {
-        if (chunk.facadeModuleId.endsWith('src/react-native/index.ts')) {
-          return 'react-native.esm.js';
-        }
-        if (chunk.facadeModuleId.endsWith('src/react/index.ts')) {
-          return 'react.esm.js';
-        }
-
-        // if (chunk.facadeModuleId.endsWith('src/react-native')) {
-        //   return 'react-native.esm.js'
-        // }
-
-        // if (chunk.facadeModuleId.endsWith('src/web')) {
-        //   return 'web.esm.js'
-        // }
-        // if (chunk.facadeModuleId.includes('/module')) {
-        //     const dir = path.dirname(chunk.facadeModuleId);
-        //     return 'module/' + path.basename(dir) + '.js';
-        // }
-      },
+module.exports = {
+  input: ['ui/list/src/react-native/index.ts', 'ui/list/src/react/index.ts'],
+  output: {
+    dir: 'dist/ui/list',
+    entryFileNames: (chunk) => {
+      if (chunk.facadeModuleId.endsWith('src/react-native/index.ts')) {
+        return 'react-native.esm.js';
+      }
+      if (chunk.facadeModuleId.endsWith('src/react/index.ts')) {
+        return 'react.esm.js';
+      }
     },
-
-    // Provide additional rollup configuration here. See: https://rollupjs.org/configuration-options
-    // e.g.
-    // output: { sourcemap: true },
-  }
-);
+  },
+};
